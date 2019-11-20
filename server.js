e2bfbf1e0f07c30c563799b6557a02c6e17298bb
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2019-11-20 00:04:15 -0500

    added node modules

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require ("push");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+ 
+
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+})
+
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
