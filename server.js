0bf119828dd35e3b3090c71f24425bdeb5ca8d15
LaMana Donadelle <36721091+lamanaml@users.noreply.github.com>
2019-11-20 01:03:33 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require ("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,6 +17,12 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
      res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+
 
 
 // Start the API server
